c59558bbfed69213fc5c44bfb6cfe1d0a773b8ef
assembleenationalerp-create <assemblee.nationale.rp@gmail.com>
2026-06-12 16:23:13 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+    intents: [
+        GatewayIntentBits.Guilds,
+        GatewayIntentBits.GuildMessages,
+        GatewayIntentBits.MessageContent
+    ]
+});
+
+client.once("ready", () => {
+    console.log(`✅ ${client.user.tag} est connecté !`);
+});
+
+client.login(process.env.TOKEN);
